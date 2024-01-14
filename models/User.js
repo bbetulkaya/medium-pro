85609f11359b1a2b678c42e7086b91a0be28c810
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2024-01-14 11:47:47 +0300

    fix: user model added id

--- models/User.js
+++ models/User.js
@@ -1,16 +1,23 @@
 const { model, models, Schema } = require("mongoose");
 
-const userSchema = Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = Schema(
+  {
+    _id: { type: Schema.Types.ObjectId, auto: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    // Include this option to allow Mongoose to automatically generate _id
+    _id: true,
+  }
+);
 
 const User = models?.User || model("User", userSchema);
 
